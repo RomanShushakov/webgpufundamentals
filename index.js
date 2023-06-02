266878d39af5d3e3f432610a24c3f1f0f95264b2
roman shushakov <roman.a.shushakov1@gmail.com>
2023-06-02 09:24:59 +0200

    Added resizeObserver to canvas element

--- index.js
+++ index.js
@@ -25,13 +25,26 @@ async function main() {
 
     const scene = await initFundamentals(device, context, gpuTextureFormat);
 
-    scene.render();
-
+    
     const input = new Float32Array([1, 3, 5, 7]);
     const output = await scene.compute(input);
-    
+
     console.log("Input:", input);
     console.log("Output:", output);
+
+
+    const observer = new ResizeObserver(entries => {
+        for (const entry of entries) {
+            const canvas = entry.target;
+            const width = entry.contentBoxSize[0].inlineSize;
+            const height = entry.contentBoxSize[0].blockSize;
+            canvas.width = Math.min(width, device.limits.maxTextureDimension2D);
+            canvas.height = Math.min(height, device.limits.maxTextureDimension2D);
+            // re-render
+            scene.render();
+        }
+    });
+    observer.observe(canvas);
 }
 
 await main();
