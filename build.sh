657839c836537f9816c4fc4dcb9ecf61133c005a
roman shushakov <roman.a.shushakov1@gmail.com>
2023-09-03 11:41:09 +0200

    updated dependencies

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-cd ./fundamentals && \
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --release --no-default-features --target wasm32-unknown-unknown && \
+cd ./wasm_modules/fundamentals && \
+cargo build --release --no-default-features --target wasm32-unknown-unknown && \
 wasm-bindgen --target web --out-name fundamentals --out-dir ../../wasm --no-typescript ./target/wasm32-unknown-unknown/release/fundamentals.wasm && \
 cd ../inter_stage_variables && \
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --release --no-default-features --target wasm32-unknown-unknown && \
+cargo build --release --no-default-features --target wasm32-unknown-unknown && \
 wasm-bindgen --target web --out-name inter_stage_variables --out-dir ../../wasm --no-typescript ./target/wasm32-unknown-unknown/release/inter_stage_variables.wasm
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import { mainFundamentals } from "./fundamentals.js";
+import { mainFundamentals } from "./fundamentals.js";
 import { mainInterStageVariables } from "./inter_stage_variables.js";
 
 
