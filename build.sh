e108c7b62c2c938bbf69b7342bc43ba1bb41e6fc
roman shushakov <roman.a.shushakov1@gmail.com>
2023-09-10 21:48:54 +0200

    updated build.sh script

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
-cd ./wasm_modules/fundamentals && \
-cargo build --release --no-default-features --target wasm32-unknown-unknown && \
-wasm-bindgen --target web --out-name fundamentals --out-dir ../../wasm --no-typescript ./target/wasm32-unknown-unknown/release/fundamentals.wasm && \
-cd ../inter_stage_variables && \
-cargo build --release --no-default-features --target wasm32-unknown-unknown && \
-wasm-bindgen --target web --out-name inter_stage_variables --out-dir ../../wasm --no-typescript ./target/wasm32-unknown-unknown/release/inter_stage_variables.wasm && \
-cd ../uniforms && \
-cargo build --release --no-default-features --target wasm32-unknown-unknown && \
-wasm-bindgen --target web --out-name uniforms --out-dir ../../wasm --no-typescript ./target/wasm32-unknown-unknown/release/uniforms.wasm && \
-cd ../storage_buffers && \
-cargo build --release --no-default-features --target wasm32-unknown-unknown && \
-wasm-bindgen --target web --out-name storage_buffers --out-dir ../../wasm --no-typescript ./target/wasm32-unknown-unknown/release/storage_buffers.wasm
+wasmModules=(
+    ./wasm_modules/fundamentals fundamentals
+    ../inter_stage_variables inter_stage_variables
+   ../uniforms uniforms
+   ../storage_buffers storage_buffers 
+)
+
+len=(${#wasmModules[@]})
+step=2
+
+for((i=0; i<len; i+=step));
+do 
+    cd ${wasmModules[i]} && \
+    cargo build --release --no-default-features --target wasm32-unknown-unknown && \
+    wasm-bindgen --target web --out-name ${wasmModules[i+1]} --out-dir ../../wasm --no-typescript ./target/wasm32-unknown-unknown/release/${wasmModules[i+1]}.wasm
+done
