80989139517684798af58e72376b1be2d0f11c6b
roman shushakov <roman.a.shushakov1@gmail.com>
2023-09-17 19:26:49 +0200

    processed 100 triangles with 1 draw call

--- wasm_modules/storage_buffers/shader/render.wgsl
+++ wasm_modules/storage_buffers/shader/render.wgsl
@@ -6,13 +6,22 @@ struct OurStruct {
 struct OtherStruct {
     scale: vec2f,
 };
+
+struct VSOutput {
+  @builtin(position) position: vec4f,
+  @location(0) color: vec4f,
+}
  
-@group(0) @binding(0) var<uniform> our_struct: OurStruct;
-@group(0) @binding(1) var<uniform> other_struct: OtherStruct;
+@group(0) @binding(0) var<storage, read> our_structs: array<OurStruct>;
+@group(0) @binding(1) var<storage, read> other_structs: array<OtherStruct>;
 
 
 @vertex 
-fn vertex_main(@builtin(vertex_index) vertex_index : u32) -> @builtin(position) vec4f
+fn vertex_main(
+    @builtin(vertex_index) vertex_index : u32,
+    @builtin(instance_index) instance_index: u32,
+) 
+    -> VSOutput
 {
     var pos = array(
         vec2(0.0, 0.5),
@@ -20,12 +29,19 @@ fn vertex_main(@builtin(vertex_index) vertex_index : u32) -> @builtin(position) 
         vec2(0.5, -0.5),
     );
 
-    return vec4f(pos[vertex_index] * other_struct.scale + our_struct.offset, 0.0, 1.0);
+    var our_struct = our_structs[instance_index];
+    var other_struct = other_structs[instance_index];
+
+    var vs_out: VSOutput;
+    vs_out.position = vec4f(
+        pos[vertex_index] * other_struct.scale + our_struct.offset, 0.0, 1.0);
+    vs_out.color = our_struct.color;
+    return vs_out;
 }
 
 
 @fragment 
-fn fragment_main() -> @location(0) vec4f
+fn fragment_main(vs_out: VSOutput) -> @location(0) vec4f
 {
-    return our_struct.color;
+    return vs_out.color;
 }
--- wasm_modules/storage_buffers/src/lib.rs
+++ wasm_modules/storage_buffers/src/lib.rs
@@ -9,7 +9,7 @@ use web_sys::
     HtmlCanvasElement,
 
 };
-use web_sys::gpu_buffer_usage::{COPY_DST, UNIFORM};
+use web_sys::gpu_buffer_usage::{COPY_DST, STORAGE};
 
 use js_sys::Float32Array;
 
@@ -51,7 +51,7 @@ impl Scene
     pub fn render(&self)
     {
         let mut render_shader_module_descriptor = GpuShaderModuleDescriptor::new(&include_str!("../shader/render.wgsl"));
-        render_shader_module_descriptor.label("triangle shaders with uniforms");
+        render_shader_module_descriptor.label("triangle shaders with storage buffers");
         let render_shader_module = self.gpu_device.create_shader_module(&render_shader_module_descriptor);
 
         let vertex_state = GpuVertexState::new("vertex_main", &render_shader_module);
@@ -62,7 +62,7 @@ impl Scene
 
         let render_layout = JsValue::from("auto");
         let mut render_pipeline_descriptor = GpuRenderPipelineDescriptor::new(&render_layout, &vertex_state);
-        render_pipeline_descriptor.label("triangle with uniforms");
+        render_pipeline_descriptor.label("triangle with storage buffers");
         render_pipeline_descriptor.fragment(&fragment_state);
         let render_pipeline = self.gpu_device.create_render_pipeline(&render_pipeline_descriptor);
 
@@ -74,72 +74,75 @@ impl Scene
                 rng.gen_range(min.unwrap()..max.unwrap())
             };
 
-        let static_uniform_buffer_size =
+        let k_num_objects = 100;
+        let mut object_infos = Vec::new();
+
+        let static_storage_unit_size =
             4 * 4 + // color is 4 32bit floats (4bytes each)
             2 * 4 + // offset is 2 32bit floats (4bytes each)
-            12 * 4;  // padding
+            2 * 4;  // padding
 
-        let uniform_buffer_size =
+        let storage_unit_size =
             2 * 4;  // scale is 2 32bit floats (4bytes each)
+
+        let static_storage_buffer_size = static_storage_unit_size * k_num_objects;
+        let changing_storage_buffer_size = storage_unit_size * k_num_objects;
+
+        let mut static_storage_buffer_descriptor = GpuBufferDescriptor::new(
+            static_storage_buffer_size.into(),
+            STORAGE | COPY_DST,
+        );
+        static_storage_buffer_descriptor.label("static storage for objects");
+        let static_storage_buffer = self.gpu_device.create_buffer(&static_storage_buffer_descriptor); 
+
+        let mut changing_storage_buffer_descriptor = GpuBufferDescriptor::new(
+            changing_storage_buffer_size.into(),
+            STORAGE | COPY_DST,
+        );
+        changing_storage_buffer_descriptor.label("changing storage for objects");
+        let changing_storage_buffer = self.gpu_device.create_buffer(&changing_storage_buffer_descriptor);
 
         let k_color_offset = 0u32;
         let k_offset_offset = 4u32;
         let k_scale_offset = 0u32;
+        
+        let static_storage_values = Float32Array::new_with_length(static_storage_buffer_size / 4);
 
-        let k_num_objects = 100;
-        let mut object_infos = Vec::new();
-
-        for i in 0..k_num_objects
+        for i in 0..k_num_objects 
         {
-            let mut static_uniform_buffer_descriptor = GpuBufferDescriptor::new(
-                static_uniform_buffer_size.into(),
-                UNIFORM | COPY_DST,
-            );
-            static_uniform_buffer_descriptor.label(&format!("static uniforms for obj: {}", i));
-            let static_uniform_buffer = self.gpu_device.create_buffer(&static_uniform_buffer_descriptor);   
+            let static_offset = i * (static_storage_unit_size / 4);
 
-            let static_uniform_values = Float32Array::new_with_length(static_uniform_buffer_size / 4);
-
+            // These are only set once so set them now
             let color = [rand(None, None), rand(None, None), rand(None, None), 1.0];
             let color_array = Float32Array::new_with_length(color.len() as u32);
             color_array.copy_from(&color);
-            static_uniform_values.set(&color_array, k_color_offset);       // set the color
-
+            static_storage_values.set(&color_array, static_offset + k_color_offset);    // set the color
             let offset = [rand(Some(-0.9), Some(0.9)), rand(Some(-0.9), Some(0.9))];
             let offset_array = Float32Array::new_with_length(offset.len() as u32);
             offset_array.copy_from(&offset);
-            static_uniform_values.set(&offset_array, k_offset_offset);     // set the offset
+            static_storage_values.set(&offset_array, static_offset + k_offset_offset);  // set the offset
 
-            // copy these values to the GPU
-            self.gpu_device.queue().write_buffer_with_u32_and_buffer_source(
-                &static_uniform_buffer, 0, &static_uniform_values,
-            );
+            object_infos.push(rand(Some(0.2), Some(0.5)));
+        }
+        self.gpu_device.queue().write_buffer_with_u32_and_buffer_source(
+            &static_storage_buffer, 0, &static_storage_values,
+        );
 
-            // create a typedarray to hold the values for the uniforms in JavaScript
-            let mut uniform_buffer_descriptor = GpuBufferDescriptor::new(
-                uniform_buffer_size.into(),
-                UNIFORM | COPY_DST,
-            );
-            uniform_buffer_descriptor.label(&format!("uniforms for obj: {}", i));
-            let uniform_buffer = self.gpu_device.create_buffer(&uniform_buffer_descriptor);
+        // a typed array we can use to update the changingStorageBuffer
+        let storage_values = Float32Array::new_with_length(changing_storage_buffer_size / 4);
 
-            let uniform_values = Float32Array::new_with_length(uniform_buffer_size / 4);
+        let bind_group_0_entry_0_resource = GpuBufferBinding::new(&static_storage_buffer);
+        let bind_group_0_entry_0 = GpuBindGroupEntry::new(0, &bind_group_0_entry_0_resource);
 
-            let bind_group_0_entry_0_resource = GpuBufferBinding::new(&static_uniform_buffer);
-            let bind_group_0_entry_0 = GpuBindGroupEntry::new(0, &bind_group_0_entry_0_resource);
-
-            let bind_group_0_entry_1_resource = GpuBufferBinding::new(&uniform_buffer);
-            let bind_group_0_entry_1 = GpuBindGroupEntry::new(1, &bind_group_0_entry_1_resource);
-        
-            let bind_group_0_entries = [bind_group_0_entry_0, bind_group_0_entry_1].iter().collect::<js_sys::Array>();
-            let mut bind_group_0_descriptor = GpuBindGroupDescriptor::new(
-                &bind_group_0_entries, &render_pipeline.get_bind_group_layout(0),
-            );
-            bind_group_0_descriptor.label(&format!("bind group 0 for obj: {}", i));
-            let bind_group_0 = self.gpu_device.create_bind_group(&bind_group_0_descriptor);
-
-            object_infos.push((rand(Some(0.2), Some(0.5)), uniform_buffer, uniform_values, bind_group_0));
-        } 
+        let bind_group_0_entry_1_resource = GpuBufferBinding::new(&changing_storage_buffer);
+        let bind_group_0_entry_1 = GpuBindGroupEntry::new(1, &bind_group_0_entry_1_resource);
+    
+        let bind_group_0_entries = [bind_group_0_entry_0, bind_group_0_entry_1].iter().collect::<js_sys::Array>();
+        let mut bind_group_0_descriptor = GpuBindGroupDescriptor::new(
+            &bind_group_0_entries, &render_pipeline.get_bind_group_layout(0),
+        );
+        bind_group_0_descriptor.label("bind group for objects");
+        let bind_group_0 = self.gpu_device.create_bind_group(&bind_group_0_descriptor);
 
         let mut color_attachment = GpuRenderPassColorAttachment::new(
             GpuLoadOp::Clear, GpuStoreOp::Store, &self.context.get_current_texture().create_view(),
@@ -158,18 +161,20 @@ impl Scene
         let canvas = self.context.canvas().dyn_into::<HtmlCanvasElement>().unwrap();
         let aspect = canvas.width() / canvas.height();
 
-        for (scale, uniform_buffer, uniform_values, bind_group_0) in object_infos 
+        for (ndx, scale) in object_infos.iter().enumerate()
         {
-            let scale = [scale / aspect as f32, scale];
-            let scale_array = Float32Array::new_with_length(scale.len() as u32);
-            scale_array.copy_from(&scale);
-            uniform_values.set(&scale_array, k_scale_offset);       // set the scale
+            let offset = ndx as u32 * (storage_unit_size / 4);
 
-            self.gpu_device.queue().write_buffer_with_u32_and_buffer_source(&uniform_buffer, 0, &uniform_values);
-
-            render_pass_encoder.set_bind_group(0, &bind_group_0);
-            render_pass_encoder.draw(3);
+            let scale_vec = [scale / aspect as f32, *scale];
+            let scale_array = Float32Array::new_with_length(scale_vec.len() as u32);
+            scale_array.copy_from(&scale_vec);
+            storage_values.set(&scale_array, offset + k_scale_offset);   // set the scale
         }
+
+        self.gpu_device.queue().write_buffer_with_u32_and_buffer_source(&changing_storage_buffer, 0, &storage_values);
+
+        render_pass_encoder.set_bind_group(0, &bind_group_0);
+        render_pass_encoder.draw_with_instance_count(3, k_num_objects);
 
         render_pass_encoder.end();
 
