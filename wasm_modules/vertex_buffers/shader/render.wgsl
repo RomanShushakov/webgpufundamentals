aaeaac6ebe4ec0520a1bbe83431c61f3daba8fdf
roman shushakov <roman.a.shushakov1@gmail.com>
2023-09-24 13:38:53 +0200

    excluded usage of storage buffers

--- wasm_modules/vertex_buffers/shader/render.wgsl
+++ wasm_modules/vertex_buffers/shader/render.wgsl
@@ -1,39 +1,25 @@
-struct OurStruct {
-    color: vec4f,
-    offset: vec2f,
-};
-
-struct OtherStruct {
-    scale: vec2f,
-};
-
 struct Vertex {
-    position: vec2f,
+    @location(0) position: vec2f,
+    @location(1) color: vec4f,
+    @location(2) offset: vec2f,
+    @location(3) scale: vec2f,
 };
 
 struct VSOutput {
   @builtin(position) position: vec4f,
   @location(0) color: vec4f,
 }
- 
-@group(0) @binding(0) var<storage, read> our_structs: array<OurStruct>;
-@group(0) @binding(1) var<storage, read> other_structs: array<OtherStruct>;
-@group(0) @binding(2) var<storage, read> pos: array<Vertex>;
 
 
 @vertex 
 fn vertex_main(
-    @builtin(vertex_index) vertex_index : u32,
-    @builtin(instance_index) instance_index: u32,
+    vert: Vertex,
 ) 
     -> VSOutput
 {
-    var our_struct = our_structs[instance_index];
-    var other_struct = other_structs[instance_index];
-
     var vs_out: VSOutput;
-    vs_out.position = vec4f(pos[vertex_index].position * other_struct.scale + our_struct.offset, 0.0, 1.0);
-    vs_out.color = our_struct.color;
+    vs_out.position = vec4f(vert.position * vert.scale + vert.offset, 0.0, 1.0);
+    vs_out.color = vert.color;
     return vs_out;
 }
 
--- wasm_modules/vertex_buffers/src/lib.rs
+++ wasm_modules/vertex_buffers/src/lib.rs
@@ -4,12 +4,11 @@ use web_sys::
 {
     GpuDevice, GpuCanvasContext, GpuTextureFormat, GpuShaderModuleDescriptor, GpuVertexState, GpuColorTargetState, 
     GpuFragmentState, GpuRenderPipelineDescriptor, GpuRenderPassColorAttachment, GpuLoadOp, GpuStoreOp, GpuColorDict, 
-    GpuRenderPassDescriptor, GpuBufferDescriptor, 
-    GpuBindGroupDescriptor, GpuBindGroupEntry, GpuBufferBinding,
-    HtmlCanvasElement,
+    GpuRenderPassDescriptor, GpuBufferDescriptor,
+    HtmlCanvasElement, GpuVertexBufferLayout, GpuVertexAttribute, GpuVertexFormat, GpuVertexStepMode,
 
 };
-use web_sys::gpu_buffer_usage::{COPY_DST, STORAGE};
+use web_sys::gpu_buffer_usage::{COPY_DST, VERTEX};
 
 use js_sys::Float32Array;
 
@@ -111,11 +110,59 @@ impl Scene
 
     pub fn render(&self)
     {
-        let mut render_shader_module_descriptor = GpuShaderModuleDescriptor::new(&include_str!("../shader/render.wgsl"));
-        render_shader_module_descriptor.label("triangle shaders with storage buffers");
-        let render_shader_module = self.gpu_device.create_shader_module(&render_shader_module_descriptor);
+        let mut render_shader_module_descriptor = GpuShaderModuleDescriptor::new(
+            &include_str!("../shader/render.wgsl"),
+        );
+        render_shader_module_descriptor.label("triangle shaders with vertex buffers");
+        let render_shader_module = self.gpu_device.create_shader_module(
+            &render_shader_module_descriptor,
+        );
 
-        let vertex_state = GpuVertexState::new("vertex_main", &render_shader_module);
+        let mut vertex_state = GpuVertexState::new("vertex_main", &render_shader_module);
+
+        let vertex_position_format = GpuVertexFormat::Float32x2;
+        let vertex_position_buffer_attribute = GpuVertexAttribute::new(
+            vertex_position_format, 0f64, 0,    // position
+        );
+        let vertex_position_buffer_attributes = [
+            vertex_position_buffer_attribute,
+        ].iter().collect::<js_sys::Array>();
+        let vertex_position_buffer_layout = GpuVertexBufferLayout::new(
+            2f64 * 4f64, &vertex_position_buffer_attributes,    // 2 floats, 4 bytes each
+        );
+
+        let vertex_color_format = GpuVertexFormat::Float32x4;
+        let vertex_color_buffer_attribute = GpuVertexAttribute::new(
+            vertex_color_format, 0f64, 1,   // color
+        );
+        let vertex_offset_format = GpuVertexFormat::Float32x2;
+        let vertex_offset_buffer_attribute = GpuVertexAttribute::new(
+            vertex_offset_format, 16f64, 2,   // offset
+        );
+        let vertex_color_offset_buffer_attributes = [
+            vertex_color_buffer_attribute, vertex_offset_buffer_attribute,
+        ].iter().collect::<js_sys::Array>();
+        let mut vertex_color_offset_buffer_layout = GpuVertexBufferLayout::new(
+            6f64 * 4f64, &vertex_color_offset_buffer_attributes,    // 6 floats, 4 bytes each
+        );
+        vertex_color_offset_buffer_layout.step_mode(GpuVertexStepMode::Instance);
+
+        let vertex_scale_format = GpuVertexFormat::Float32x2;
+        let vertex_scale_buffer_attribute = GpuVertexAttribute::new(
+            vertex_scale_format, 0f64, 3,    // scale
+        );
+        let vertex_scale_buffer_attributes = [
+            vertex_scale_buffer_attribute,
+        ].iter().collect::<js_sys::Array>();
+        let mut vertex_scale_buffer_layout = GpuVertexBufferLayout::new(
+            2f64 * 4f64, &vertex_scale_buffer_attributes,    // 2 floats, 4 bytes each
+        );
+        vertex_scale_buffer_layout.step_mode(GpuVertexStepMode::Instance);
+
+        let vertex_buffers = [
+            vertex_position_buffer_layout, vertex_color_offset_buffer_layout, vertex_scale_buffer_layout,
+        ].iter().collect::<js_sys::Array>();
+        vertex_state.buffers(&vertex_buffers);
 
         let color_target_state = GpuColorTargetState::new(self.gpu_texture_format);
         let fragment_state_targets = [color_target_state].iter().collect::<js_sys::Array>();
@@ -123,97 +170,78 @@ impl Scene
 
         let render_layout = JsValue::from("auto");
         let mut render_pipeline_descriptor = GpuRenderPipelineDescriptor::new(&render_layout, &vertex_state);
-        render_pipeline_descriptor.label("triangle with storage buffers");
+        render_pipeline_descriptor.label("triangle with vertex buffers");
         render_pipeline_descriptor.fragment(&fragment_state);
         let render_pipeline = self.gpu_device.create_render_pipeline(&render_pipeline_descriptor);
 
         let k_num_objects = 100;
         let mut object_infos = Vec::new();
 
-        let static_storage_unit_size =
+        let static_unit_size =
             4 * 4 + // color is 4 32bit floats (4bytes each)
-            2 * 4 + // offset is 2 32bit floats (4bytes each)
-            2 * 4;  // padding
+            2 * 4;  // offset is 2 32bit floats (4bytes each)
 
-        let storage_unit_size =
+        let changing_unit_size =
             2 * 4;  // scale is 2 32bit floats (4bytes each)
 
-        let static_storage_buffer_size = static_storage_unit_size * k_num_objects;
-        let changing_storage_buffer_size = storage_unit_size * k_num_objects;
+        let static_vertex_buffer_size = static_unit_size * k_num_objects;
+        let changing_vertex_buffer_size = changing_unit_size * k_num_objects;
 
-        let mut static_storage_buffer_descriptor = GpuBufferDescriptor::new(
-            static_storage_buffer_size.into(),
-            STORAGE | COPY_DST,
+        let mut static_vertex_buffer_descriptor = GpuBufferDescriptor::new(
+            static_vertex_buffer_size.into(),
+            VERTEX | COPY_DST,
         );
-        static_storage_buffer_descriptor.label("static storage for objects");
-        let static_storage_buffer = self.gpu_device.create_buffer(&static_storage_buffer_descriptor); 
+        static_vertex_buffer_descriptor.label("static storage for objects");
+        let static_vertex_buffer = self.gpu_device.create_buffer(&static_vertex_buffer_descriptor); 
 
-        let mut changing_storage_buffer_descriptor = GpuBufferDescriptor::new(
-            changing_storage_buffer_size.into(),
-            STORAGE | COPY_DST,
+        let mut changing_vertex_buffer_descriptor = GpuBufferDescriptor::new(
+            changing_vertex_buffer_size.into(),
+            VERTEX | COPY_DST,
         );
-        changing_storage_buffer_descriptor.label("changing storage for objects");
-        let changing_storage_buffer = self.gpu_device.create_buffer(&changing_storage_buffer_descriptor);
+        changing_vertex_buffer_descriptor.label("changing storage for objects");
+        let changing_vertex_buffer = self.gpu_device.create_buffer(&changing_vertex_buffer_descriptor);
 
         let k_color_offset = 0u32;
         let k_offset_offset = 4u32;
         let k_scale_offset = 0u32;
         
-        let static_storage_values = Float32Array::new_with_length(static_storage_buffer_size / 4);
+        let static_vertex_values = Float32Array::new_with_length(static_vertex_buffer_size / 4);
 
         for i in 0..k_num_objects 
         {
-            let static_offset = i * (static_storage_unit_size / 4);
+            let static_offset = i * (static_unit_size / 4);
 
             // These are only set once so set them now
             let color = [rand(None, None), rand(None, None), rand(None, None), 1.0];
             let color_array = Float32Array::new_with_length(color.len() as u32);
             color_array.copy_from(&color);
-            static_storage_values.set(&color_array, static_offset + k_color_offset);    // set the color
+            static_vertex_values.set(&color_array, static_offset + k_color_offset);    // set the color
             let offset = [rand(Some(-0.9), Some(0.9)), rand(Some(-0.9), Some(0.9))];
             let offset_array = Float32Array::new_with_length(offset.len() as u32);
             offset_array.copy_from(&offset);
-            static_storage_values.set(&offset_array, static_offset + k_offset_offset);  // set the offset
+            static_vertex_values.set(&offset_array, static_offset + k_offset_offset);  // set the offset
 
             object_infos.push(rand(Some(0.2), Some(0.5)));
         }
         self.gpu_device.queue().write_buffer_with_u32_and_buffer_source(
-            &static_storage_buffer, 0, &static_storage_values,
+            &static_vertex_buffer, 0, &static_vertex_values,
         );
 
         // a typed array we can use to update the changingStorageBuffer
-        let storage_values = Float32Array::new_with_length(changing_storage_buffer_size / 4);
+        let changing_vertex_values = Float32Array::new_with_length(changing_vertex_buffer_size / 4);
 
         // setup a storage buffer with vertex data
         let (vertex_data, num_vertices) = create_circle_vertices(Some(0.5), Some(0.25));
 
-        let mut vertex_storage_buffer_descriptor = GpuBufferDescriptor::new(
+        let mut vertex_buffer_descriptor = GpuBufferDescriptor::new(
             vertex_data.byte_length().into(),
-            STORAGE | COPY_DST,
+            VERTEX | COPY_DST,
         );
-        vertex_storage_buffer_descriptor.label("storage buffer vertices");
-        let vertex_storage_buffer = self.gpu_device.create_buffer(&vertex_storage_buffer_descriptor);
+        vertex_buffer_descriptor.label("vertex buffer vertices");
+        let vertex_buffer = self.gpu_device.create_buffer(&vertex_buffer_descriptor);
         self.gpu_device.queue().write_buffer_with_u32_and_buffer_source(
-            &vertex_storage_buffer, 0, &vertex_data,
+            &vertex_buffer, 0, &vertex_data,
         );
-
-        let bind_group_0_entry_0_resource = GpuBufferBinding::new(&static_storage_buffer);
-        let bind_group_0_entry_0 = GpuBindGroupEntry::new(0, &bind_group_0_entry_0_resource);
-
-        let bind_group_0_entry_1_resource = GpuBufferBinding::new(&changing_storage_buffer);
-        let bind_group_0_entry_1 = GpuBindGroupEntry::new(1, &bind_group_0_entry_1_resource);
-
-        let bind_group_0_entry_2_resource = GpuBufferBinding::new(&vertex_storage_buffer);
-        let bind_group_0_entry_2 = GpuBindGroupEntry::new(2, &bind_group_0_entry_2_resource);
-    
-        let bind_group_0_entries = [
-            bind_group_0_entry_0, bind_group_0_entry_1, bind_group_0_entry_2,
-        ].iter().collect::<js_sys::Array>();
-        let mut bind_group_0_descriptor = GpuBindGroupDescriptor::new(
-            &bind_group_0_entries, &render_pipeline.get_bind_group_layout(0),
-        );
-        bind_group_0_descriptor.label("bind group for objects");
-        let bind_group_0 = self.gpu_device.create_bind_group(&bind_group_0_descriptor);
 
         let mut color_attachment = GpuRenderPassColorAttachment::new(
             GpuLoadOp::Clear, GpuStoreOp::Store, &self.context.get_current_texture().create_view(),
@@ -228,22 +256,26 @@ impl Scene
 
         let render_pass_encoder = command_encoder.begin_render_pass(&render_pass_descriptor);
         render_pass_encoder.set_pipeline(&render_pipeline);
+        render_pass_encoder.set_vertex_buffer(0, &vertex_buffer);
+        render_pass_encoder.set_vertex_buffer(1, &static_vertex_buffer);
+        render_pass_encoder.set_vertex_buffer(2, &changing_vertex_buffer);
 
         let canvas = self.context.canvas().dyn_into::<HtmlCanvasElement>().unwrap();
         let aspect = canvas.width() / canvas.height();
 
         for (ndx, scale) in object_infos.iter().enumerate()
         {
-            let offset = ndx as u32 * (storage_unit_size / 4);
+            let offset = ndx as u32 * (changing_unit_size / 4);
 
             let scale_vec = [scale / aspect as f32, *scale];
             let scale_array = Float32Array::new_with_length(scale_vec.len() as u32);
             scale_array.copy_from(&scale_vec);
-            storage_values.set(&scale_array, offset + k_scale_offset);   // set the scale
+            changing_vertex_values.set(&scale_array, offset + k_scale_offset);   // set the scale
         }
-        self.gpu_device.queue().write_buffer_with_u32_and_buffer_source(&changing_storage_buffer, 0, &storage_values);
+        self.gpu_device.queue().write_buffer_with_u32_and_buffer_source(
+            &changing_vertex_buffer, 0, &changing_vertex_values,
+        );
 
-        render_pass_encoder.set_bind_group(0, &bind_group_0);
         render_pass_encoder.draw_with_instance_count(num_vertices, k_num_objects);
 
         render_pass_encoder.end();
