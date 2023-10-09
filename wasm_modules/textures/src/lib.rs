fd3d3928760403c35be61d3c224ec3ca9d9777aa
roman shushakov <roman.a.shushakov1@gmail.com>
2023-10-09 18:41:41 +0200

    added f letter texture

--- wasm_modules/textures/src/lib.rs
+++ wasm_modules/textures/src/lib.rs
@@ -4,7 +4,13 @@ use web_sys::
 {
     GpuDevice, GpuCanvasContext, GpuTextureFormat, GpuShaderModuleDescriptor, GpuVertexState, GpuColorTargetState, 
     GpuFragmentState, GpuRenderPipelineDescriptor, GpuRenderPassColorAttachment, GpuLoadOp, GpuStoreOp, GpuColorDict, 
-    GpuRenderPassDescriptor,
+    GpuRenderPassDescriptor, GpuTextureDescriptor, GpuImageCopyTexture, GpuImageDataLayout, GpuExtent3dDict,
+    GpuBindGroupEntry, GpuBindGroupDescriptor,
+};
+
+use web_sys::gpu_texture_usage::
+{
+    TEXTURE_BINDING, COPY_DST,
 };
 
 #[wasm_bindgen]
@@ -61,9 +67,57 @@ impl Scene
 
         let render_layout = JsValue::from("auto");
         let mut render_pipeline_descriptor = GpuRenderPipelineDescriptor::new(&render_layout, &vertex_state);
-        render_pipeline_descriptor.label("hardcoded checkerboard triangle pipeline");
+        render_pipeline_descriptor.label("hardcoded textured quad pipeline");
         render_pipeline_descriptor.fragment(&fragment_state);
         let render_pipeline = self.gpu_device.create_render_pipeline(&render_pipeline_descriptor);
+
+        let k_texture_width = 5;
+        let k_texture_height = 7;
+
+        let r = [255, 0, 0, 255];  // red
+        let y = [255, 255, 0, 255];  // yellow
+        let b = [0, 0, 255, 255];  // blue
+
+        let texture_data = [
+                b, r, r, r, r,
+                r, y, y, y, r,
+                r, y, r, r, r,
+                r, y, y, r, r,
+                r, y, r, r, r,
+                r, y, r, r, r,
+                r, r, r, r, r,
+            ].into_iter().flatten().collect::<Vec<u8>>();
+
+        let texture_descriptor = GpuTextureDescriptor::new(
+            GpuTextureFormat::Rgba8unorm,
+            &[k_texture_width, k_texture_height].iter().copied().map(JsValue::from).collect::<js_sys::Array>(),
+            TEXTURE_BINDING | COPY_DST,
+        );
+        let texture = self.gpu_device.create_texture(&texture_descriptor);
+
+        let gpu_image_copy_texture = GpuImageCopyTexture::new(&texture);
+        let mut gpu_image_data_layout = GpuImageDataLayout::new();
+        gpu_image_data_layout.bytes_per_row(k_texture_width * 4);
+        let mut gpu_extent_3d_dict = GpuExtent3dDict::new(k_texture_width);
+        gpu_extent_3d_dict.height(k_texture_height);
+
+        self.gpu_device.queue().write_texture_with_u8_array_and_gpu_extent_3d_dict(
+            &gpu_image_copy_texture, 
+            &texture_data, 
+            &gpu_image_data_layout, 
+            &gpu_extent_3d_dict,
+        );
+
+        let sampler = self.gpu_device.create_sampler();
+
+        let bind_group_0_entry_0 = GpuBindGroupEntry::new(0, &sampler);
+        let bind_group_0_entry_1 = GpuBindGroupEntry::new(1, &texture.create_view());
+        let bind_group_0_entries = [bind_group_0_entry_0, bind_group_0_entry_1].iter().collect::<js_sys::Array>();
+
+        let bind_group_0_descriptor = GpuBindGroupDescriptor::new(
+            &bind_group_0_entries, &render_pipeline.get_bind_group_layout(0),
+        );
+        let bind_group_0 = self.gpu_device.create_bind_group(&bind_group_0_descriptor);
 
         let mut color_attachment = GpuRenderPassColorAttachment::new(
             GpuLoadOp::Clear, GpuStoreOp::Store, &self.context.get_current_texture().create_view(),
@@ -75,7 +129,8 @@ impl Scene
 
         let render_pass_encoder = command_encoder.begin_render_pass(&render_pass_descriptor);
         render_pass_encoder.set_pipeline(&render_pipeline);
-        render_pass_encoder.draw(3);
+        render_pass_encoder.set_bind_group(0, &bind_group_0);
+        render_pass_encoder.draw(6);
         render_pass_encoder.end();
 
 
