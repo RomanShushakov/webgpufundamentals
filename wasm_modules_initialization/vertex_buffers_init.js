1d43f59f2c501e8d2f9ad181de3d922a3ebc0ed3
Roman <52500180+RomanShushakov@users.noreply.github.com>
2025-01-16 13:01:39 +0100

    Delete wasm_modules_initialization/vertex_buffers_init.js

--- wasm_modules_initialization/vertex_buffers_init.js
+++
@@ -1,8 +0,0 @@
-import init, { Scene } from "../wasm/vertex_buffers.js";
-
-
-export async function initVertexBuffers(device, context, gpuTextureFormat) {
-    await init();
-    const scene = Scene.create(device, context, gpuTextureFormat);
-    return scene;    
-}
