65e2e52e04a18345a03ebcd5f1f6449ff75143f2
Roman <52500180+RomanShushakov@users.noreply.github.com>
2025-01-16 13:02:02 +0100

    Delete wasm_modules_initialization/storage_buffers_init.js

--- wasm_modules_initialization/storage_buffers_init.js
+++
@@ -1,8 +0,0 @@
-import init, { Scene } from "../wasm/storage_buffers.js";
-
-
-export async function initStorageBuffers(device, context, gpuTextureFormat) {
-    await init();
-    const scene = Scene.create(device, context, gpuTextureFormat);
-    return scene;    
-}
