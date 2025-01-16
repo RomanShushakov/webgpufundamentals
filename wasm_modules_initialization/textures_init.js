d537e49e5029687413fd5d4596cf3cac24d9db69
Roman <52500180+RomanShushakov@users.noreply.github.com>
2025-01-16 13:01:54 +0100

    Delete wasm_modules_initialization/textures_init.js

--- wasm_modules_initialization/textures_init.js
+++
@@ -1,8 +0,0 @@
-import init, { Scene } from "../wasm/textures.js";
-
-
-export async function initTextures(device, context, gpuTextureFormat) {
-    await init();
-    const scene = Scene.create(device, context, gpuTextureFormat);
-    return scene;    
-}
